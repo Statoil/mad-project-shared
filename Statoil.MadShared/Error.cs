a17a450d53d2c02d7cfccb3c4e55887850b90f8a
Vladimir Veremjov <vladimirvv@gmail.com>
2017-02-27 14:54:42 +0100

    Initial push
    Created solution with a class library project. Added two classes.

---
+++ Statoil.MadShared/Error.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statoil.MadShared
+{
+    public class Error
+    {
+        public int Code { get; internal set; }
+        public string Message { get; set; }
+        public string System { get; set; }
+    }
+}
---
+++ Statoil.MadShared/Reference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statoil.MadShared
+{
+    public class Reference
+    {
+        public string Link { get; set; }
+        public string Value { get; set; }
+    }
+}
